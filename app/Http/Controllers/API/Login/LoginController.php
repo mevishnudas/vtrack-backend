fd9763aa76676848518ca17fec335cb2b5fb0cac
Vishnu Das <dvishnudas@gmail.com>
2025-12-02 23:37:27 +0530

    first commit

---
+++ app/Http/Controllers/API/Login/LoginController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\API\Login;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login(Request $request){
+        $data = array();
+        $data["name"] = "Vishnu";
+        $data["token"] = "123xxxx123xxxx123xxx123";
+        return response(["status"=>200,"msg"=>"Success","data"=>$data],200);
+    }
+}
