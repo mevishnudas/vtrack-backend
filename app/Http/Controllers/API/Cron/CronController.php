c31e54ae86e85cb4ba5e9f7aa9f90cb7c37d65cc
Vishnu Das <vishnudas@popitsolutions.com>
2026-04-23 17:07:57 +0530

    cron time update

--- app/Http/Controllers/API/Cron/CronController.php
+++ app/Http/Controllers/API/Cron/CronController.php
@@ -11,7 +11,8 @@ class CronController extends Controller
     public function run(){
 
         //Run Account Summary at 06:00 AM
-        if (now()->format('H:i') === '06:00') {
+        $current_time = now()->format('H:i');
+        if ($current_time === '06:00'|| $current_time === '18:00') {
             RepaymentController::accountSummary();
         }
 
