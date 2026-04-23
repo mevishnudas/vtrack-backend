ec00874a643ee76628c9c620ef7e9edfc5ef9097
Vishnu Das <dvishnudas@gmail.com>
2026-04-23 22:37:52 +0530

    cron : time update

--- app/Http/Controllers/API/Cron/CronController.php
+++ app/Http/Controllers/API/Cron/CronController.php
@@ -5,17 +5,19 @@ namespace App\Http\Controllers\API\Cron;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\Repayment\RepaymentController;
+use Illuminate\Support\Facades\Log;
 
 class CronController extends Controller
 {
     public function run(){
 
-        //Run Account Summary at 06:00 AM
+        //Run Account Summary at 06:00 AM & 6:00 PM
         $current_time = now()->format('H:i');
-        if ($current_time === '06:00'|| $current_time === '18:00') {
+        //Log::info("CRON TIME: " . $current_time);
+        if ($current_time === '05:30'|| $current_time === '18:30') {
             RepaymentController::accountSummary();
         }
 
-        return response(["msg"=>"Success"],200);
+        return response(["msg"=>"Success","data"=>$current_time],200);
     }
 }
